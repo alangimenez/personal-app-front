9fa9188940f4b0ddc33f6cb9394fe235b7450a0e
Alan Gimenez <gimenezalan00@gmail.com>
2023-01-31 18:32:08 -0300

    migrate project to frontend service

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import { BrowserRouter, Routes, Route, Switch } from 'react-router-dom'
+import Demo from './Components/Demo';
+import Ingresos from './Components/Ingresos';
+import Gastos from './Components/Gastos';
+import Inversiones from './Components/Inversiones'
+import Cashflow from './Components/Cashflow';
+
+function App() {
+
+  const path = 'https://7lgu80.deta.dev'
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Demo path={path} />}></Route>
+        <Route path="/ingresos" element={<Ingresos path={path} />}></Route>
+        <Route path="/gastos" element={<Gastos path={path} />}></Route>
+        <Route path="/inversiones" element={<Inversiones path={path} />} />
+        <Route path="/cashflow" element={<Cashflow path={path} />}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App;
