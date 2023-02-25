6e45545170e9154df1d3d5ddc4f64fcbf5c1b1cb
AlanGimenez <gimenezalan00@gmail.com>
2023-02-25 18:50:49 -0300

    modal for save expenses in credit card working

--- src/Components/Expenses/CreditCard/ModalNewCreditCardExpense.js
+++ src/Components/Expenses/CreditCard/ModalNewCreditCardExpense.js
@@ -1,81 +1,124 @@
-import { useEffect, useState } from "react"
+import DataContext from "../../Context/Context"
+import InfoMessage from "../../Utils/InfoMessage"
 import LabelInput from "../../Utils/LabelInput"
-import ModalBody from "../../Utils/ModalBody"
+import LabelTextArea from "../../Utils/LabelTextArea"
 import ModalButton from "../../Utils/ModalButton"
 import Select from "../../Utils/Select"
-import InfoMessage from "../../Utils/InfoMessage"
+import ModalBody from "../../Utils/ModalBody"
+import { useState, useEffect, useContext } from "react"
 import NewInputs from "../../Expenses/NewInputs"
 
-function ModalNewCreditCardExpense({path}) {
+function ModalNewCreditCardExpense({ path }) {
 
-    const [liquidAccounts, setLiquidAccounts] = useState([])
-    const getLiquidAccounts = () => {
-        fetch(`${path}/account/liquid-fci`)
-            .then(res => res.json())
-            .then(data => {
-                const array = []
-                data.map(d => array.push(d.name))
-                setLiquidAccounts(array)
-            })
-    }
+    const { items, resetItems } = useContext(DataContext)
 
-    const saveCreditCard = () => {
+    const saveExpenseInCreditCard = () => {
         document.getElementById('new-expense-credit-card-close').disabled = true
         document.getElementById('new-expense-credit-card-save').disabled = true
         document.getElementById('new-expense-credit-card-msg').style.display = "unset"
+
+        const accountsAmounts = []
+        for (let i = 0; i < items; i++) {
+            const eachExpense = {
+                "account": document.getElementById(`account${i}`).value,
+                "debtAmount": document.getElementById(`amount${i}`).value,
+                "discountAmount": document.getElementById(`discount${i}`).value
+            }
+            accountsAmounts.push(eachExpense)
+        }
+
         const requestOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 "name": document.getElementById("new-expense-credit-card-name").value,
-                "debtAccount": document.getElementById("new-expense-credit-card-debt-account").value
+                "date": document.getElementById("new-expense-credit-card-date").value,
+                "expenses": accountsAmounts,
+                "currency": 'ARS',
+                "comments": document.getElementById("new-expense-credit-card-comments").value,
+                "period": document.getElementById("new-expense-credit-card-period").value
             })
         }
 
-        fetch(`${path}/creditcard`, requestOptions)
-            .then(res => res.json())
-            .then(data => {
-                document.getElementById('new-expense-credit-card-msg').innerHTML = 'La cuenta fue creada con éxito'
+        fetch(`${path}/expensecreditcard`, requestOptions)
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data)
+                document.getElementById('new-expense-credit-card-msg').innerHTML = 'El gasto fue guardado con éxito'
                 document.getElementById('new-expense-credit-card-msg').className = 'alert alert-success'
-
                 setTimeout(() => {
-                    document.getElementById('new-expense-credit-card-msg').innerHTML = 'Estamos creando la tarjeta'
+                    document.getElementById('new-expense-credit-card-msg').innerHTML = 'Estamos guardando el gasto'
                     document.getElementById('new-expense-credit-card-msg').style.display = "none"
-                    document.getElementById('new-expense-credit-card-name').value = ""
-                    document.getElementById('new-expense-credit-card-debt-account').value = ""
                     document.getElementById('new-expense-credit-card-close').disabled = false
                     document.getElementById('new-expense-credit-card-save').disabled = false
+
+                    document.getElementById("new-expense-credit-card-date").value = ""
+                    document.getElementById("new-expense-credit-card-comments").value = ""
+                    document.getElementById("new-expense-credit-card-period").value = ""
+                    const parent = document.getElementById('newInputsRoot')
+                    while (parent.firstChild) {
+                        parent.firstChild.remove()
+                    }
+                    resetItems()
                 }, 2000)
             })
     }
 
-    useEffect(() => { getLiquidAccounts() }, [])
+    const [creditCard, setCreditCard] = useState([])
+    const [creditCardNames, setCreditCardNames] = useState([])
+    const [period, setPeriod] = useState([])
+
+    const getOpenPeriodByCreditCard = () => {
+        fetch(`${path}/expensecreditcard/period`)
+            .then(res => res.json())
+            .then(data => {
+                setCreditCard(data)
+                const array = []
+                data.map(it => array.push(it.name))
+                setCreditCardNames(array)
+                setPeriod(data[0].openPeriods)
+            })
+    }
+
+    useEffect(() => { getOpenPeriodByCreditCard() }, [])
+
+    const handleChangeSelectA = (event) => {
+        const key = creditCard.findIndex(at => at.name == event.target.value)
+        setPeriod(creditCard[key].openPeriods)
+    }
 
     return (
-        <>
+        <div>
             <ModalButton target={'#new-expense-credit-card'}>
-                Crear nuevo gasto con tarjeta
+                Ingresar nuevo gasto con tarjeta
             </ModalButton>
             <ModalBody id={'new-expense-credit-card'}>
-                <form>
-                    <Select text={'Tarjeta'} id={'new-expense-credit-card-name'} options={['Tarjeta 1', 'Tarjeta 2']} />
-                    <LabelInput text={'Fecha del gasto'} id={'new-expense-credit-card-date'} type={'date'} />
-                    <Select text={'Periodo'} id={'new-expense-credit-card-period'} options={['Periodo 1', 'Periodo 2']} />
-                    <LabelInput text={'Cantidad de cuotas'} id={'new-expense-credit-payments'} type={'number'} />
-                    <NewInputs path={path} />
-                </form>
+                <>
+                    <form>
+                        <LabelInput text={'Fecha'} id={'new-expense-credit-card-date'} type={'date'} />
+                        {/* <Select text={'Tarjeta'} id={'new-expense-credit-card-name'} options={creditCardNames} onChange={handleChangeSelectA}/> */}
+                        <div className="form-group">
+                            <label htmlFor='new-expense-credit-card-name'>Asset type</label>
+                            <select className="form-control" id='new-expense-credit-card-name' onChange={handleChangeSelectA}>
+                                {creditCardNames.map(opt => <option>{opt}</option>)}
+                            </select>
+                        </div>
+                        <Select text={'Periodo'} id={'new-expense-credit-card-period'} options={period} />
+                        <LabelTextArea text={'Comentarios'} id={'new-expense-credit-card-comments'} />
+                        <NewInputs path={path} />
+                    </form>
 
-                <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-dismiss="modal" id="new-expense-credit-card-close">Close</button>
-                    <button type="button" className="btn btn-primary" onClick={saveCreditCard} id="new-expense-credit-card-save">Save changes</button>
-                </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal" id="new-expense-credit-card-close">Close</button>
+                        <button type="button" className="btn btn-primary" onClick={saveExpenseInCreditCard} id="new-expense-credit-card-save">Save changes</button>
+                    </div>
 
-                <InfoMessage id={'new-expense-credit-card-msg'} type='alert alert-info'>
-                    Estamos guardando el gasto
-                </InfoMessage>
-
+                    <InfoMessage type='alert alert-info' id={'new-expense-credit-card-msg'}>
+                        Estamos guardando el gasto
+                    </InfoMessage>
+                </>
             </ModalBody>
-        </>
+        </div>
     )
 }
 
