c36058281bb6c8b979df6fd18606ceeb7e511e5e
Alan Gimenez <gimenezalan00@gmail.com>
2023-03-17 22:54:12 -0300

    add fields to select by year and month

--- src/Components/CreditCard/TableCreditCardExpenses.js
+++ src/Components/CreditCard/TableCreditCardExpenses.js
@@ -3,18 +3,18 @@ import { useEffect, useState } from "react"
 
 function TableCreditCardExpenses({ path }) {
 
-    const [creditCardDetail, setCreditCardDetail] = useState([])
+    const [periodsOfCreditCards, setPeriodsOfCreditCards] = useState([])
     const [creditCardNames, setCreditCardNames] = useState([])
-    const getExpensesByCreditCard = () => {
-        fetch(`${path}/expensecreditcard`)
+    const getPeriodsOfCreditCard = () => {
+        fetch(`${path}/expensecreditcard/period/ALL`)
             .then(res => res.json())
             .then(data => {
-                setCreditCardDetail(data)
-                getCreditCardNames(data)
+                setPeriodsOfCreditCards(data)
+                getDataOfCreditCards(data)
             })
     }
 
-    const getCreditCardNames = (data) => {
+    const getDataOfCreditCards = data => {
         const arrayOfNames = []
         data.map(ccd => {
             if (!arrayOfNames.includes(ccd.name)) {
@@ -22,46 +22,39 @@ function TableCreditCardExpenses({ path }) {
             }
         })
         setCreditCardNames(arrayOfNames)
-
-        getPeriofOfEachCreditCard(arrayOfNames, data)
+        
     }
 
-    const [creditCardPeriods, setCreditCardPeriods] = useState([])
-    const getPeriofOfEachCreditCard = (arrayNames, data) => {
-        const arrayOfPeriodByCreditCard = []
-        arrayNames.map(an => {
-            const period = []
-            data.map(d => {
-                if (d.name == an) {
-                    period.push(d.period)
-                }
-            })
-            arrayOfPeriodByCreditCard.push({
-                "name": an,
-                "periods": period
-            })
-        })
-        setCreditCardPeriods(arrayOfPeriodByCreditCard)
-    }
-
-    useEffect(() => { getExpensesByCreditCard() }, [])
+    useEffect(() => { getPeriodsOfCreditCard() }, [])
 
     const [year, setYear] = useState([])
     const [month, setMonth] = useState([])
     const [keyOfCreditCard, setKeyOfCreditCard] = useState(0)
-    const handleChangeSelectCreditCard = event => {
-        const key = creditCardPeriods.findIndex(ccp => ccp.name == event.target.value)
-        setKeyOfCreditCard(key)
-        const arrayOfYears = []
-        console.log(creditCardPeriods[key])
-        creditCardPeriods[key].openPeriods.map(p => arrayOfYears.push(p.year))
-        setYear(arrayOfYears)
+    const handleChangeSelectCreditCard = event => changeCreditCard(event.target.value)
+
+    const changeCreditCard = name => {
+        const {key, year} = changeArrayOfYears(name)
+        changeArrayOfMonths(year, key)
     }
 
-    const handleChangeSelectYear = (event) => {
-        const keyOfPeriod = creditCardPeriods[keyOfCreditCard].openPeriods.findIndex(p => p.year == event.target.value)
+    const handleChangeSelectYear = (event) => changeArrayOfMonths(event.target.value, keyOfCreditCard)
+
+    const changeArrayOfYears = name => {
+        const key = periodsOfCreditCards.findIndex(ccp => ccp.name == name)
+        setKeyOfCreditCard(key)
+        const arrayOfYears = []
+        periodsOfCreditCards[key].openPeriods.map(p => arrayOfYears.push(p.year))
+        setYear(arrayOfYears)
+        return {
+            "key": key,
+            "year": arrayOfYears[0]
+        }
+    }
+
+    const changeArrayOfMonths = (year, key) => {
+        const keyOfPeriod = periodsOfCreditCards[key].openPeriods.findIndex(p => p.year == year)
         const arrayOfMonths = []
-        creditCardPeriods[keyOfCreditCard].openPeriods[keyOfPeriod].month.map(p => arrayOfMonths.push(p))
+        periodsOfCreditCards[key].openPeriods[keyOfPeriod].month.map(p => arrayOfMonths.push(p))
         setMonth(arrayOfMonths)
     }
 
@@ -72,19 +65,23 @@ function TableCreditCardExpenses({ path }) {
     const [totalAmount, setTotalAmount] = useState(0)
     const showExpenses = () => {
         const name = document.getElementById('table-credit-card-expenses-name').value
-        const period = document.getElementById('table-credit-card-expenses-period').value
+        const year = document.getElementById('table-credit-card-expenses-year').value
+        const month = document.getElementById('table-credit-card-expenses-month').value
 
-        const key = creditCardDetail.findIndex(ccd => ccd.name == name && ccd.period == period)
-        setExpenses(creditCardDetail[key].expenses)
-        setStatus(creditCardDetail[key].status)
-        setCloseDate(creditCardDetail[key].closeDate)
-        setPaymentDate(creditCardDetail[key].paymentDate)
+        fetch(`${path}/expensecreditcard/expenses?name=${name}&year=${year}&month=${month}`)
+            .then(res => res.json())
+            .then(data => {
+                setExpenses(data[0].expenses)
+                setStatus(data[0].status)
+                setCloseDate(data[0].closeDate)
+                setPaymentDate(data[0].paymentDate)
 
-        let total = 0
-        creditCardDetail[key].expenses.map(e => {
-            total = total + e.amount
-        })
-        setTotalAmount(total)
+                let total = 0
+                data[0].expenses.map(e => {
+                    total = total + e.amount
+                })
+                setTotalAmount(total)
+            })
     }
 
     return (
@@ -97,15 +94,15 @@ function TableCreditCardExpenses({ path }) {
                     </select>
                 </div>
                 <div className="form-group">
-                    <label htmlFor='new-expense-credit-card-name'>Año</label>
-                    <select className="form-control" id='new-expense-credit-card-name' onChange={handleChangeSelectCreditCard}>
-                        {year.map(opt => <option>{opt}</option>)}
+                    <label htmlFor='table-credit-card-expenses-year'>Año</label>
+                    <select className="form-control" id='table-credit-card-expenses-year' onChange={handleChangeSelectYear}>
+                        {year.map((opt, index) => <option key={index}>{opt}</option>)}
                     </select>
                 </div>
                 <div className="form-group">
-                    <label htmlFor='new-expense-credit-card-year'>Mes</label>
-                    <select className="form-control" id='new-expense-credit-card-year' onChange={handleChangeSelectYear}>
-                        {month.map(opt => <option>{opt}</option>)}
+                    <label htmlFor='table-credit-card-expenses-month'>Mes</label>
+                    <select className="form-control" id='table-credit-card-expenses-month'>
+                        {month.map((opt, index) => <option key={index}>{opt}</option>)}
                     </select>
                 </div>
                 <button type="button" className="btn btn-primary" id="table-credit-card-expenses-get" onClick={showExpenses}>Mostrar</button>
