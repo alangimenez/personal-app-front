83ab83af84fe1394a1092eab791522992218759c
Alan Gimenez <gimenezalan00@gmail.com>
2023-03-19 14:38:24 -0300

    refactor modal new investment to save a new register

--- src/Components/Investments/modalNewInvestment.js
+++ src/Components/Investments/modalNewInvestment.js
@@ -4,73 +4,133 @@ import Select from "../Utils/Select"
 import ModalBody from "../Utils/ModalBody"
 import { useState, useEffect } from 'react'
 import InfoMessage from "../Utils/InfoMessage"
+import LabelTextArea from "../Utils/LabelTextArea"
 
 function ModalNewInvestment({ path }) {
+    const btnClose = document.getElementById('modal-new-investment-btn-close')
+    const btnSave = document.getElementById('modal-new-investment-btn-save')
+    const infoMessage = document.getElementById('modal-new-investment-msg')
+    const ticket = document.getElementById("modal-new-investment-ticket")
+    const purchaseDate = document.getElementById("modal-new-investment-purchase-date")
+    const quantity = document.getElementById("modal-new-investment-quantity")
+    const purchasePrice = document.getElementById("modal-new-investment-purchase-price")
+    const currency = document.getElementById("modal-new-investment-currency")
+    const assetType = document.getElementById("modal-new-investment-asset-type")
+    const operation = document.getElementById("modal-new-investment-operation")
+    const commision = document.getElementById("modal-new-investment-commission")
+    const account = document.getElementById("modal-new-investment-account")
+    const comments = document.getElementById("modal-new-investment-comments")
 
-    // save investment register
     const saveInvestment = () => {
 
-        document.getElementById('new-investment-btn-close').disabled = true
-        document.getElementById('new-investment-btn-save').disabled = true
-        document.getElementById('new-investment-msg').style.display = "unset"
+        btnClose.disabled = true
+        btnSave.disabled = true
+        infoMessage.style.display = "unset"
 
+        // first fetch (save investment)
         const requestOptionsInvestment = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                "ticket": document.getElementById("ticket").value,
-                "operationDate": document.getElementById("purchaseDate").value,
-                "operationQuantity": document.getElementById("quantity").value,
-                "operationPrice": document.getElementById("purchasePrice").value,
-                "operationCurrency": document.getElementById("currency").value,
-                "assetType": document.getElementById("assetType").value,
-                "operation": document.getElementById("operation").value,
-                "actualQuantity": document.getElementById("quantity").value,
-                "commission": document.getElementById("commission").value,
-                "commissionCurrency": document.getElementById("currency").value
+                "ticket": ticket.value,
+                "operationDate": purchaseDate.value,
+                "operationQuantity": quantity.value,
+                "operationPrice": purchasePrice.value,
+                "operationCurrency": currency.value,
+                "assetType": assetType.value,
+                "operation": operation.value,
+                "actualQuantity": quantity.value,
+                "commission": commision.value,
+                "commissionCurrency": currency.value
             })
         }
 
         fetch(`${path}/investment`, requestOptionsInvestment)
             .then((res) => res.json())
-            .then((data) => {
-                console.log(data)
-                document.getElementById('new-investment-msg').innerHTML = 'Estamos guardando la cotización de compra'
+            .then(() => {
+                infoMessage.innerHTML = 'Estamos guardando la cotización de compra'
             })
 
+        // second fetch (save last value)
         const requestOptionsLastValue = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
-                "ticket": document.getElementById('ticket').value,
-                "price": document.getElementById('purchasePrice').value,
+                "ticket": ticket.value,
+                "price": purchasePrice.value,
                 "volume": 0
             })
         };
 
         fetch(`${path}/lastvalue/manualquote`, requestOptionsLastValue)
             .then(res => res.json())
-            .then(data => {
-                document.getElementById('new-investment-msg').innerHTML = 'La inversión fue guardada con éxito'
-                document.getElementById('new-investment-msg').className = 'alert alert-success'
+            .then(() => {
+                infoMessage.innerHTML = 'Estamos creando el registro de inversión'
+            })
+
+        // third fetch (save register)
+        let creditAccount
+        let debitAccount
+        if (operation.value === "Buy") {
+            creditAccount = ticket.value
+            debitAccount = account.value
+        } else {
+            creditAccount = account.value
+            debitAccount = ticket.value
+        }
+
+        const accountsAmounts = {
+            "debtAccount": creditAccount,
+            "discountAmount": 0,
+            "debtAmount": purchasePrice.value * quantity.value
+        }
+
+        const requestOptionsRegister = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                "date": purchaseDate.value,
+                "expenses": [accountsAmounts],
+                "credit": debitAccount,
+                "currency": currency.value,
+                "comments": comments.value,
+                "benefitMP": false,
+                "investment": true,
+                "operation": operation.value
+            })
+        }
+
+        fetch(`${path}/registers/batch`, requestOptionsRegister)
+            .then((res) => res.json())
+            .then(() => {
+                infoMessage.className = 'alert alert-success'
+                infoMessage.innerHTML = 'La inversión fue guardada con éxito'
 
                 setTimeout(() => {
-                    document.getElementById('new-investment-msg').innerHTML = 'Estamos creando el tipo de activo'
-                    document.getElementById('new-investment-msg').style.display = "none"
-                    document.getElementById('new-investment-btn-close').disabled = false
-                    document.getElementById('new-investment-btn-save').disabled = false
-                    document.getElementById("ticket").value = ""
-                    document.getElementById("purchaseDate").value = ""
-                    document.getElementById("quantity").value = ""
-                    document.getElementById("purchasePrice").value = ""
-                    document.getElementById("currency").value = ""
-                    document.getElementById("assetType").value = ""
-                    document.getElementById("operation").value = ""
-                    document.getElementById("commission").value = ""
+                    infoMessage.innerHTML = 'Estamos guardando la inversión'
+                    infoMessage.style.display = "none"
+                    btnClose.disabled = false
+                    btnSave.disabled = false
+                    ticket.value = ""
+                    purchaseDate.value = ""
+                    quantity.value = ""
+                    purchasePrice.value = ""
+                    currency.value = ""
+                    assetType.value = ""
+                    operation.value = ""
+                    commision.value = ""
+                    comments.value = ""
                 }, 2000)
             })
     }
 
+    // handlers
+    const handleChangeSelect = (event) => {
+        const key = assetTypeInfo.findIndex(at => at.assetType == event.target.value)
+        setListOfAssets(assetTypeInfo[key].assets)
+    }
+
+    // useEffect functions
     const [assetTypeInfo, setAssetTypeInfo] = useState([])
     const [assetTypes, setAssetTypes] = useState([])
     const [listOfAssets, setListOfAssets] = useState([])
@@ -86,20 +146,22 @@ function ModalNewInvestment({ path }) {
             })
     }
 
-    useEffect(() => { getAssetsTypes() }, [])
-
-    const handleChangeSelect = (event) => {
-        const key = assetTypeInfo.findIndex(at => at.assetType == event.target.value)
-        setListOfAssets(assetTypeInfo[key].assets)
+    const [liquidAccounts, setLiquidAccounts] = useState([])
+    const getLiquidAccounts = () => {
+        fetch(`${path}/account/liquid`)
+            .then(res => res.json())
+            .then(data => setLiquidAccounts(data))
     }
+
+    useEffect(() => { getAssetsTypes(); getLiquidAccounts() }, [])
 
     return (
         <div>
-            <ModalButton target={'#exampleModal'}>
+            <ModalButton target={'#modal-new-investment'}>
                 Ingresar nueva inversión
             </ModalButton>
 
-            <ModalBody id={'exampleModal'}>
+            <ModalBody id={'modal-new-investment'}>
                 <div className="modal-header">
                     <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
                     <button type="button" className="close" data-dismiss="modal" aria-label="Close">
@@ -108,33 +170,34 @@ function ModalNewInvestment({ path }) {
                 </div>
                 <div className="modal-body">
                     <form>
-                        {/* <Select text={'Asset type'} id={'assetType'} options={assetTypes} /> */}
                         <div className="form-group">
-                            <label htmlFor='assetType'>Asset type</label>
-                            <select className="form-control" id='assetType' onChange={handleChangeSelect}>
+                            <label htmlFor='modal-new-investment-asset-type'>Asset type</label>
+                            <select className="form-control" id='modal-new-investment-asset-type' onChange={handleChangeSelect}>
                                 {assetTypes.map((opt, index) => <option key={index}>{opt}</option>)}
                             </select>
                         </div>
                         <div className="form-group">
-                            <label htmlFor='ticket'>Ticket</label>
-                            <select className="form-control" id='ticket'>
+                            <label htmlFor='modal-new-investment-ticket'>Ticket</label>
+                            <select className="form-control" id='modal-new-investment-ticket'>
                                 {listOfAssets.map((opt, index) => <option key={index}>{opt}</option>)}
                             </select>
                         </div>
-                        <LabelInput text={'Purchase date'} id={'purchaseDate'} type={'date'} />
-                        <LabelInput text={'Quantity'} id={'quantity'} type={'number'} />
-                        <LabelInput text={'Purchase price'} id={'purchasePrice'} type={'number'} />
-                        <LabelInput text={'Commissions'} id={'commission'} type={'number'} />
-                        <Select text={'Currency'} id={'currency'} options={['USD', 'ARS']} />
-                        <Select text={'Operation'} id={'operation'} options={['Buy', 'Sell']} />
+                        <LabelInput text={'Purchase date'} id={'modal-new-investment-purchase-date'} type={'date'} />
+                        <LabelInput text={'Quantity'} id={'modal-new-investment-quantity'} type={'number'} />
+                        <LabelInput text={'Purchase price'} id={'modal-new-investment-purchase-price'} type={'number'} />
+                        <LabelInput text={'Commissions'} id={'modal-new-investment-commission'} type={'number'} />
+                        <Select text={'Currency'} id={'modal-new-investment-currency'} options={['USD', 'ARS']} />
+                        <Select text={'Operation'} id={'modal-new-investment-operation'} options={['Buy', 'Sell']} />
+                        <Select text={'Cuenta débito/crédito'} id={'modal-new-investment-account'} options={liquidAccounts} />
+                        <LabelTextArea text={'Comentarios'} id={'modal-new-investment-comments'} />
 
                     </form>
                 </div>
                 <div className="modal-footer">
-                    <button type="button" className="btn btn-secondary" data-dismiss="modal" id="new-investment-btn-close">Close</button>
-                    <button type="button" className="btn btn-primary" onClick={saveInvestment} id="new-investment-btn-save">Save changes</button>
+                    <button type="button" className="btn btn-secondary" data-dismiss="modal" id="modal-new-investment-btn-close">Close</button>
+                    <button type="button" className="btn btn-primary" onClick={saveInvestment} id="modal-new-investment-btn-save">Save changes</button>
                 </div>
-                <InfoMessage id={'new-investment-msg'} type={'alert alert-info'}>
+                <InfoMessage id={'modal-new-investment-msg'} type={'alert alert-info'}>
                     Estamos guardando la inversión
                 </InfoMessage>
             </ModalBody>
